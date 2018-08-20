d7d68643709335515eecbf75d4660d2818b7817b
Ilia <plue@inbox.ru>
2018-08-20 11:09:35 +0300

    include templates

--- gulpfile.js
+++ gulpfile.js
@@ -14,129 +14,174 @@ var include = require('posthtml-include'); // инклюдит одни файл
 var run = require('run-sequence');
 var del = require('del');
 
+
+// var sourcemaps = require('gulp-sourcemaps');
+// var concat = require('gulp-concat');
+// var uglify = require('uglify-js');   - вроде можно и без concat!!!!
+
+// gulp.task('js', function () {
+//   gulp.src(path.src.js)
+//     .pipe(sourcemaps.init())
+//     .pipe(uglify())
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest(path.build.js))
+//     .pipe(reload({
+//       stream: true
+//     }));
+// });
+
+// var concat = require('gulp-concat');
+
+// gulp.task('scripts', function () {
+//   gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js'])
+//     .pipe(concat('all.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest('./dist/'))
+// });
+
+// gulp.task('js:build', function () {
+//   gulp.src(path.src.js) //Найдем наш main файл
+//     .pipe(rigger()) //Прогоним через rigger
+//     .pipe(sourcemaps.init()) //Инициализируем sourcemap
+//     .pipe(uglify()) //Сожмем наш js
+//     .pipe(sourcemaps.write()) //Пропишем карты
+//     .pipe(rename({
+//       suffix: '.min'
+//     })) //добавим суффикс .min к выходному файлу
+//     .pipe(gulp.dest(path.build.js)) //выгрузим готовый файл в build
+//     .pipe(connect.reload()) //И перезагрузим сервер
+// });
+
 // установить SVGsprite инлайново в любом файле
 //<div style="display:none">
 //<include src="build/img/sprite.svg"></include>
 //</div>
 
-var config = {
-    src: '.',
-    build: './build',
-    html: {
-        src: '/*.html',
-        dest: '/'
-    },
-    style: {
-        watch: '/sass/**/*.scss',
-        src: '/sass/style.scss',
-        dest: '/css/'
-    },
-    fonts: {
-        // src: '/fonts/**.{woff,woff2}',
-        src: '/fonts/**',
-        dest: '/fonts/',
+//<include src = "../templates/header.html"></include>
+//<include src="../templates/footer.html"></include>
 
-    },
-    js: {
-        src: '/js/**',
-        dest: '/js/'
-    },
-    css: {
-        src: '/css/*',
-        dest: '/css/'
-    },
-    img: {
-        src: '/img/**',
-        watch: '/img/**/*.{png,jpg,svg}',
-        webp: '/img/**/*.{png,jpg}', // для преобразования в webp 
-        dest: '/img/'
-    },
-    svgsprite: {
-        src: '/img/icon-*.svg', //бере svg для спрайта 
-        dest: '/img/'
-    }
+var config = {
+  src: '.',
+  build: './build',
+  html: {
+    src: '/*.html',
+    dest: '/'
+  },
+  style: {
+    watch: '/sass/**/*.scss',
+    src: '/sass/style.scss',
+    dest: '/css/'
+  },
+  fonts: {
+    // src: '/fonts/**.{woff,woff2}',
+    src: '/fonts/**',
+    dest: '/fonts/',
+
+  },
+  js: {
+    src: '/js/**',
+    dest: '/js/'
+  },
+  css: {
+    src: '/css/*',
+    dest: '/css/'
+  },
+  img: {
+    src: '/img/**',
+    watch: '/img/**/*.{png,jpg,svg}',
+    webp: '/img/**/*.{png,jpg}', // для преобразования в webp
+    dest: '/img/'
+  },
+  svgsprite: {
+    src: '/img/icons/icon-*.svg', //бере svg для спрайта
+    dest: '/img/'
+  }
 };
 
 gulp.task('style', function () {
-    gulp.src(config.src + config.style.src)
-        //из-за бага с plumber тут return нет 
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(postcss([
-            autoprefixer()
-        ]))
-        .pipe(gulp.dest(config.build + config.style.dest))
-        .pipe(minify())
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest(config.build + config.style.dest))
-        .pipe(server.stream());
+  gulp.src(config.src + config.style.src)
+    //из-за бага с plumber тут return нет
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['> 0.1%'],
+        cascade: false
+      })
+    ]))
+    .pipe(gulp.dest(config.build + config.style.dest))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest(config.build + config.style.dest))
+    .pipe(server.stream());
 });
 
 gulp.task('images', function () {
-    return gulp.src(config.src + config.img.watch)
-        .pipe(imagemin([
-            imagemin.optipng({
-                optimizationLevel: 3
-            }),
-            imagemin.jpegtran({
-                progressive: true
-            }),
-            imagemin.svgo({
-                plugins: [{
-                        removeViewBox: false
-                    },
-                    {
-                        cleanupIDs: false
-                    }
-                ]
-            }),
-            {
-                verbose: true
-            }
+  return gulp.src(config.src + config.img.watch)
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: false
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      }),
+      {
+        verbose: true
+      }
 
-        ]))
-        .pipe(gulp.dest(config.src + config.img.dest));
+    ]))
+    .pipe(gulp.dest(config.src + config.img.dest));
 });
 
 gulp.task('webp', function () {
-    return gulp.src(config.src + config.img.webp)
-        .pipe(webp({
-            quality: 90
-        }))
-        .pipe(gulp.dest(config.src + config.img.dest));
+  return gulp.src(config.src + config.img.webp)
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest(config.src + config.img.dest));
 });
 
 gulp.task('sprite', function () {
-    return gulp.src(config.src + config.svgsprite.src)
-        .pipe(svgstore({
-            inLineSvg: true
-        }))
-        .pipe(rename('sprite.svg'))
-        .pipe(gulp.dest(config.build + config.svgsprite.dest));
+  return gulp.src(config.src + config.svgsprite.src)
+    .pipe(svgstore({
+      inLineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest(config.build + config.svgsprite.dest));
 });
 
 gulp.task('html', function () {
-    return gulp.src(config.src + config.html.src)
-        .pipe(posthtml([
-            include()
-        ]))
-        .pipe(gulp.dest(config.build));
+  return gulp.src(config.src + config.html.src)
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest(config.build));
 });
 
-// копирование ??
+// копирование
 gulp.task('copy', function () {
-    return gulp.src([
-            config.src + config.fonts.src,
-            config.src + config.img.src,
-            config.src + config.js.src,
-        ], {
-            base: config.src
-        })
-        .pipe(gulp.dest(config.build + config.html.dest));
+  return gulp.src([
+      config.src + config.fonts.src,
+      config.src + config.img.src,
+      config.src + config.js.src,
+      config.src + '/robots.txt'
+    ], {
+      base: config.src
+    })
+    .pipe(gulp.dest(config.build + config.html.dest));
 });
 
 gulp.task('clean', function () {
-    return del('build');
+  return del('build');
 });
 
 // gulp.task('serve', ['style'], function () {
@@ -150,22 +195,22 @@ gulp.task('clean', function () {
 // });
 
 gulp.task('serve', function () {
-    server.init({
-        server: config.build
-    });
+  server.init({
+    server: config.build
+  });
 
-    gulp.watch(config.src + config.style.watch, ['style']);
-    gulp.watch(config.src + config.html.src, ['html']);
+  gulp.watch(config.src + config.style.watch, ['style']);
+  gulp.watch(config.src + config.html.src, ['html']);
 
 });
 
 gulp.task('build', function (done) {
-    run(
-        'clean',
-        'copy',
-        'style',
-        'sprite',
-        'html',
-        done
-    );
+  run(
+    'clean',
+    'copy',
+    'style',
+    'sprite',
+    'html',
+    done
+  );
 });
